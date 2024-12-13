c5fc19fc995837ba1a630b72a67be022c3268ccc
TomashevskiyDP <goofi170192@gmail.com>
2024-12-13 08:37:43 +0500

    Add files via upload

---
+++ peremen.py
@@ -0,0 +1,13 @@
+# Практическое задание по работе в Pycharm - "Переменные"
+
+
+number_of_homework_tasks_completed = 12
+number_of_hours_spent = 1.5
+course_name = 'Python'
+time_per_task = number_of_hours_spent / number_of_homework_tasks_completed
+print(f'Курс: {course_name}, всего задач: {number_of_homework_tasks_completed}, затрачено часов: {number_of_hours_spent}, среднее время выполнения: {time_per_task},часа.')
+
+
+
+
+
