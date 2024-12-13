94bf3f37272c1f5554bebd119c71b20563bf2311
TomashevskiyDP <goofi170192@gmail.com>
2024-12-13 09:08:33 +0500

    Add files via upload

---
+++ stroki.py
@@ -0,0 +1,9 @@
+# Практическое задание по уроку "Строки и индексация строк"
+
+example = 'Котопёс'
+print(example[0])
+print(example[-1])
+print(example[4:])
+print(example[::-1])
+print(example[::2])
+
